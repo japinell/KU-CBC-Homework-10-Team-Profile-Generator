5390ba6698efd70bb57f0157abf9d4e83272f8ab
JAPinell <japinell@yahoo.com>
2021-05-21 23:52:12 -0500

    Added roster.html file

--- __tests__/Generator.test.js
+++ __tests__/Generator.test.js
@@ -9,15 +9,15 @@ describe("GIVEN a command-line application that accepts user input", () => {
     //
     test("THEN an HTML file is generated that displays a nicely formatted team roster based on user input", () => {
       //
-      // Mock the "writeToFile" function, which generates the team roster based on user input
-      //
       const fileName = "roster.html";
       const data = {};
       const gen = new Generator(fileName, data);
       //
       expect(gen).toBeInstanceOf(Generator);
       expect(gen.getFileName()).toEqual(fileName);
-      expect(gen.getData()).toEqual(data);
+      expect(gen.getMarkdown()).toEqual(data);
+      //
+      // TODO: Mock the "writeToFile" function, which generates the team roster based on user input
       //
     });
     //
